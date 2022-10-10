d12fecd3169c8a48c0ea6791a8bf11bd763faa59
310440501RSL211122 <310440501RSL211122@LB329-02>
2022-10-10 09:33:12 +0300

    Add project files.

---
+++ lab 2 - calc/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace lab_2___calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNumber1 = new System.Windows.Forms.Label();
+            this.labelNumber2 = new System.Windows.Forms.Label();
+            this.textBoxNumber2 = new System.Windows.Forms.TextBox();
+            this.buttonSum = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonx = new System.Windows.Forms.Button();
+            this.buttondiv = new System.Windows.Forms.Button();
+            this.textBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelNumber1
+            // 
+            this.labelNumber1.AutoSize = true;
+            this.labelNumber1.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumber1.Location = new System.Drawing.Point(84, 52);
+            this.labelNumber1.Name = "labelNumber1";
+            this.labelNumber1.Size = new System.Drawing.Size(111, 27);
+            this.labelNumber1.TabIndex = 0;
+            this.labelNumber1.Text = "Number 1";
+            // 
+            // labelNumber2
+            // 
+            this.labelNumber2.AutoSize = true;
+            this.labelNumber2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNumber2.Location = new System.Drawing.Point(322, 52);
+            this.labelNumber2.Name = "labelNumber2";
+            this.labelNumber2.Size = new System.Drawing.Size(113, 25);
+            this.labelNumber2.TabIndex = 1;
+            this.labelNumber2.Text = "Number 2";
+            // 
+            // textBoxNumber2
+            // 
+            this.textBoxNumber2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumber2.Location = new System.Drawing.Point(335, 111);
+            this.textBoxNumber2.Name = "textBoxNumber2";
+            this.textBoxNumber2.Size = new System.Drawing.Size(100, 33);
+            this.textBoxNumber2.TabIndex = 3;
+            // 
+            // buttonSum
+            // 
+            this.buttonSum.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSum.Location = new System.Drawing.Point(127, 182);
+            this.buttonSum.Name = "buttonSum";
+            this.buttonSum.Size = new System.Drawing.Size(75, 34);
+            this.buttonSum.TabIndex = 4;
+            this.buttonSum.Text = "+";
+            this.buttonSum.UseVisualStyleBackColor = true;
+            this.buttonSum.Click += new System.EventHandler(this.buttonSum_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMinus.Location = new System.Drawing.Point(356, 182);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(75, 35);
+            this.buttonMinus.TabIndex = 5;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonx
+            // 
+            this.buttonx.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonx.Location = new System.Drawing.Point(127, 256);
+            this.buttonx.Name = "buttonx";
+            this.buttonx.Size = new System.Drawing.Size(75, 31);
+            this.buttonx.TabIndex = 6;
+            this.buttonx.Text = "x";
+            this.buttonx.UseVisualStyleBackColor = true;
+            this.buttonx.Click += new System.EventHandler(this.buttonx_Click);
+            // 
+            // buttondiv
+            // 
+            this.buttondiv.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttondiv.Location = new System.Drawing.Point(356, 256);
+            this.buttondiv.Name = "buttondiv";
+            this.buttondiv.Size = new System.Drawing.Size(75, 31);
+            this.buttondiv.TabIndex = 7;
+            this.buttondiv.Text = ":";
+            this.buttondiv.UseVisualStyleBackColor = true;
+            this.buttondiv.Click += new System.EventHandler(this.buttondiv_Click);
+            // 
+            // textBoxNumber1
+            // 
+            this.textBoxNumber1.Location = new System.Drawing.Point(89, 120);
+            this.textBoxNumber1.Name = "textBoxNumber1";
+            this.textBoxNumber1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNumber1);
+            this.Controls.Add(this.buttondiv);
+            this.Controls.Add(this.buttonx);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonSum);
+            this.Controls.Add(this.textBoxNumber2);
+            this.Controls.Add(this.labelNumber2);
+            this.Controls.Add(this.labelNumber1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNumber1;
+        private System.Windows.Forms.Label labelNumber2;
+        private System.Windows.Forms.TextBox textBoxNumber2;
+        private System.Windows.Forms.Button buttonSum;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonx;
+        private System.Windows.Forms.Button buttondiv;
+        private System.Windows.Forms.TextBox textBoxNumber1;
+    }
+}
+
---
+++ lab 2 - calc/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2___calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSum_Click(object sender, EventArgs e)
+        {
+            byte first;
+            byte second;
+            try
+            {
+                first = byte.Parse(textBoxNumber1.Text);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("First number should be an double");
+                return;
+            }
+
+            try
+            {
+                second = byte.Parse(textBoxNumber2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Second number should be an double");
+                return;
+            }
+
+            int result = first + second;
+            MessageBox.Show(result.ToString());
+
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            double first;
+            double second;
+            bool isParasablefirst = double.TryParse(textBoxNumber1.Text, out first);
+            if (!isParasablefirst)
+            {
+                MessageBox.Show("First number should be a double!");
+                return;
+            }
+            bool isParasablesecond = double.TryParse(textBoxNumber2.Text, out second);
+            if (!isParasablesecond)
+            {
+                MessageBox.Show("Second number should be a double!");
+                return;
+            }
+            double result = first - second;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void buttonx_Click(object sender, EventArgs e)
+        {
+            double first;
+            double second;
+            bool isParasablefirst = double.TryParse(textBoxNumber1.Text, out first);
+            if (!isParasablefirst)
+            {
+                MessageBox.Show("First number should be a double!");
+                return;
+            }
+            bool isParasablesecond = double.TryParse(textBoxNumber2.Text, out second);
+            if (!isParasablesecond)
+            {
+                MessageBox.Show("Second number should be a double!");
+                return;
+            }
+            double result = first * second;
+            MessageBox.Show(result.ToString());
+        }
+
+        private void buttondiv_Click(object sender, EventArgs e)
+        {
+            double first;
+            double second;
+            bool isParasablefirst = double.TryParse(textBoxNumber1.Text, out first);
+            if (!isParasablefirst)
+            {
+                MessageBox.Show("First number should be a double!");
+                return;
+            }
+            bool isParasablesecond = double.TryParse(textBoxNumber2.Text, out second);
+            if (!isParasablesecond)
+            {
+                MessageBox.Show("Second number should be a double!");
+                return;
+            }
+            if (second == 0)
+            {
+                MessageBox.Show("Division by zero!");
+                return;
+            }
+            double result = first / second;
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
